868ac60c6fd14563859d527f2b06ef3dce554ae8
t6lepoit <touanti18@gmail.com>
2020-06-20 12:39:44 +0200

    simplifies code

--- bot.py
+++ bot.py
@@ -2,15 +2,17 @@
 import os
 
 import discord
-from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = 'NzIzODM2OTExMDE3Mzk0MjM4.Xu3b3w.k4vavWFI3OxXMUsc1Hdv7BOXcUo'
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
 
 client.run(TOKEN)
