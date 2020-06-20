74bebeb09cd6eaf975f79f774222968b08d5eb82
t6lepoit <touanti18@gmail.com>
2020-06-20 12:45:07 +0200

    adding message

--- bot.py
+++ bot.py
@@ -1,11 +1,23 @@
 # bot.py
 import os
+import asyncio
 
 import discord
 
 TOKEN = 'NzIzODM2OTExMDE3Mzk0MjM4.Xu3b3w.k4vavWFI3OxXMUsc1Hdv7BOXcUo'
 
 client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('_hein'):
+        msg = ':heeee: :eeee: :eeee: :eeee: :ein:'.format(message)
+        await message.channel.send(msg)
+
 
 @client.event
 async def on_ready():
