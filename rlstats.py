d6aca35373b5437e66e62c417912b15926aea533
t6lepoit <touanti18@gmail.com>
2020-08-27 20:25:22 +0200

    Fast Push

--- rlstats.py
+++ rlstats.py
@@ -6,10 +6,14 @@ import calendar
 import discord
 import os
 import asyncio
+from urllib.request import Request, urlopen
 
 def test(message,guild) :
     if message.attachments :
-        data = pd.read_csv(message.attachments[0].url)
+        req = Request(message.attachments[0].url)
+        req.add_header('User-Agent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0')
+        content = urlopen(req)
+        data = pd.read_csv(content)
         print(data)
     else :
         print ("no attachment")
