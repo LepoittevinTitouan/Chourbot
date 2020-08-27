344b556a9a89e0cf9796b58759e1f711f7944b00
t6lepoit <touanti18@gmail.com>
2020-08-27 20:28:45 +0200

    Fast Push

--- rlstats.py
+++ rlstats.py
@@ -14,6 +14,6 @@ def test(message,guild) :
         req.add_header('User-Agent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0')
         content = urlopen(req)
         data = pd.read_csv(content)
-        print(data)
+        user = message.author
     else :
-        print ("no attachment")
+        print (message.author)
