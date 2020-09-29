f138d1e5acefb4591ad77322f53c7f13e866480a
t6lepoit <touanti18@gmail.com>
2020-09-30 00:18:43 +0200

    Fast Push

--- rlstats.py
+++ rlstats.py
@@ -36,6 +36,8 @@ async def call(message,guild) :
             await plot3s(data,message)
         elif "2s" in message.content :
             await plot2s(data,message)
+        elif "1s" in message.content :
+            await plot1s(data,message)
         else :
             await plotRecap(data,message)
 
@@ -398,6 +400,160 @@ async def plot2s(data,message):
     #Emplacement annotations : x = 75 et y = 1 et 0
     axMVP.annotate(str(int(mvpMean)) + " % of total games", (75,1),va='center',ha='center')
     axMVP.annotate(str(int(mvpMeanWin)) + " % of total wins",(75,0),va='center',ha='center')
+
+    #Best day
+    axBestDay = fig.add_subplot(gs[5,4:6])
+    bDayString = "Best day :\n" + str(bDay) + " (" + str(int(bDayWinrate)) + "%)"
+    axBestDay.text(0.5,0.5,bDayString,va='center',ha='center')
+    axBestDay.axis("off")
+
+    #Best hour
+    axBestHour = fig.add_subplot(gs[6,4:6])
+    bHourString = "Best hour :\n" + str(bHour) + " (" + str(int(bHourWinrate)) + "%)"
+    axBestHour.text(0.5,0.5,bHourString,va='center',ha='center')
+    axBestHour.axis("off")
+
+    # Saving and sending the file
+    fig.savefig('fig1.png')
+
+    file = discord.File('fig1.png')
+    embed = discord.Embed()
+    embed.set_image(url="attachment://fig1.png")
+    await message.channel.send(file=file,embed=embed)
+
+async def plot1s(data,message):
+
+    #Traitement données :
+    #Limitation du nombre de données
+    data = data.loc[data["Playlist"] == "Duel"]
+    data = data.loc[data["Ranked"] == 1]
+
+    if len(message.content.split()) > 2 :
+        dt = datetime.strptime(message.content.split()[2], '%Y-%m-%d')
+        data = data.loc[data["Timestamp"] > dt]
+
+    if data.empty :
+        await message.channel.send("No game played in 1s in " + str(message.author) + "' saved file.")
+        return
+
+    #Selection du MMR avec exclusion des buggés
+    mmr1s = data.loc[data["MMR"] > 200]
+    mmr1s = mmr1s["MMR"].tolist()
+
+    #Selection du score
+    score = data["Score"].tolist()
+    scoremean = data["Score"].mean()
+
+    #Selection des MVP
+    mvp = data["MVP"].tolist()
+
+    #Fluctuation du MMR
+    mmrwin = 0
+    mmrloose = 0
+    prec = 0
+    for i in mmr1s :
+        if not prec :
+            prec = i
+        elif i > prec :
+            mmrwin = mmrwin + (i - prec)
+            prec = i
+        elif i < prec :
+            mmrloose = mmrloose + ( prec - i )
+            prec = i
+        else :
+            prec = i
+
+    #Préparation du pie chart
+    labels = 'Goals','Saves','Assists'
+    sizes =[]
+    for l in labels :
+        myList = data[l].tolist()
+        total = sum(myList)
+        sizes.append(total)
+
+    # Winrate + win/loose
+    winrate = data["Win"].mean()
+    winrate = winrate * 100
+    win = data["Win"].sum()
+    loose = (win * 100 / winrate) - win
+    nb = win + loose
+
+    goalsTot = sum(data['Goals'].tolist())/nb
+    savesTot = sum(data['Saves'].tolist())/nb
+
+    #Best day & best hour
+    days = data.groupby("Weekday").mean()
+    bDay = days["Win"].idxmax()
+    bDayWinrate = days["Win"].max()
+    bDayWinrate = bDayWinrate * 100
+
+    data["Rounded"] = data["Timestamp"].dt.round('H')
+    data["Rounded"] = data["Rounded"].dt.time
+    hours = data.groupby("Rounded").mean()
+    bHour = hours["Win"].idxmax()
+    bHourWinrate = hours["Win"].max()
+    bHourWinrate = bHourWinrate * 100
+
+
+    # -- PLOT --
+    plt.rc('figure',facecolor='xkcd:greyblue')
+    fig = plt.figure(constrained_layout = True)
+    # Use GridSpec for customising layout
+    gs = fig.add_gridspec(nrows=7, ncols=6)
+    # Add an empty axes that occupied the whole first row
+    axTitle = fig.add_subplot(gs[0,:])
+    axTitle.text(0.5,0.5,'1s Data',va='center', ha='center')
+    axTitle.axis("off")
+
+    # Plot principal
+    axPrincipal = fig.add_subplot(gs[1:3,0:4])
+    line1, = axPrincipal.plot(mmr1s,label = "1s")
+
+    # Annotation fluctuation
+    axFlucPos = fig.add_subplot(gs[1,4:6])
+    toShowPositiveMMR = "MMR total gagné :\n" + str(mmrwin)
+    axFlucPos.text(0.5,0.5,toShowPositiveMMR,va='center',ha='center')
+    axFlucPos.axis("off")
+
+    # Annotation fluctuation
+    axFlucNeg = fig.add_subplot(gs[2,4:6])
+    toShowNegativeMMR = "MMR total perdu :\n" + str(mmrloose)
+    axFlucNeg.text(0.5,0.5,toShowNegativeMMR,va='center',ha='center')
+    axFlucNeg.axis("off")
+
+    # Pie chart
+    axPie = fig.add_subplot(gs[3:5,0:2])
+    axPie.pie(sizes,labels=labels,autopct='%d%%')
+    axPie.axis('equal')
+
+    # Annotation Winrate
+    axWinrate = fig.add_subplot(gs[3,2:4])
+    winrateper = "Winrate :\n" + str(int(winrate)) + "%"
+    axWinrate.text(0.5,0.5,winrateper,va='center',ha='center')
+    axWinrate.axis("off")
+
+    # Annotation win & loose
+    axWin = fig.add_subplot(gs[4,2])
+    winNb = str(int(win)) + "\nWins"
+    axWin.text(0.5,0.5,winNb,va='center',ha='center')
+    axWin.axis("off")
+
+    axLoose = fig.add_subplot(gs[4,3])
+    loosNb = str(int(loose)) + "\nLooses"
+    axLoose.text(0.5,0.5,loosNb,va='center',ha='center')
+    axLoose.axis("off")
+
+    # Annotation nombre goals shots
+    axTot = fig.add_subplot(gs[3,4:6])
+    goalsSavesTot = "Total Goals : " + str(round(goalsTot,2)) + "\nTotal Saves : " + str(round(savesTot,2))
+    axTot.text(0.5,0.5,goalsSavesTot,va='center',ha='center')
+    axTot.axis("off")
+
+    # Annotation nb de parties
+    axNb = fig.add_subplot(gs[4,4:6])
+    nbParties = str(int(nb)) + "\nParties jouées"
+    axNb.text(0.5,0.5,nbParties,va='center',ha='center')
+    axNb.axis("off")
 
     #Best day
     axBestDay = fig.add_subplot(gs[5,4:6])
