e075328965ff868d2527743c528b4a06833d4a92
Titouan L <touanti18@gmail.com>
2022-04-27 22:10:57 +0200

    Nettoyage de printemps

--- bot.py
+++ bot.py
@@ -1,67 +1,37 @@
-import os,sys
+import os, sys
 import time
 import random
 import discord
-import sqlite3
 from dotenv import load_dotenv
 
-import csstats
+import json
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-
-burgerMembers = []
 connectedGuild = None
 # Load commands
-import json
+
 with open('command.json') as f:
-  commands = json.load(f)
+    commands = json.load(f)
 
 # Import all callbacks
 from callbacks import *
 
-
-# Connect to DB
-conn = sqlite3.connect('users.db')
-c = conn.cursor()
-try :
-    # Create table
-    c.execute('''CREATE TABLE users
-                 (id text, name text, display_name text)''')
-    conn.commit()
-except:
-    print("Table already created")
-
-
-
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
-    global connectedGuild,burgerMembers
+    global connectedGuild
     for guild in client.guilds:
         if guild.name == GUILD:
-            connectedGuild=guild
+            connectedGuild = guild
             break
-
-    # burgerRole = guild.roles
-    # print("Burger :",burgerRole)
-    burgerRole = guild.get_role(730562433365835857)
-    burgerMembers = burgerRole.members
-
-    print("\nOnline burger member :")
-    for member in burgerMembers:
-        if member.status != discord.Status.offline:
-            print(member.display_name)
-
-
-    # Get all members
-    # members = '\n - '.join([member.display_name for member in guild.members])
-    # print(f'Guild Members:\n - {members}')
-
     print("Ready !")
+
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -74,30 +44,33 @@ async def on_message(message):
         splitedString = message.content.split()
 
         if splitedString[0] == command["code"]:
-            print("Start function :",splitedString[0])
+            print("Start function :", splitedString[0])
 
             # Start a function from a string
             possibles = globals().copy()
             possibles.update(locals())
             method = possibles.get(command["callback"])
             if not method:
-                 raise NotImplementedError("Method %s not implemented" % method_name)
-            await method(message,connectedGuild)
+                raise NotImplementedError("Method %s not implemented" % method)
+            await method(message, connectedGuild)
 
 
 @client.event
-async def on_voice_state_update(member,before,after):
-    if  str(member.id)!="523531083078434819": # Exclute the bot itself
-        if (after.channel.name=="Heiiiiiiiiiin" and (before.channel==None or before.channel.name!="Heiiiiiiiiiin")):
+async def on_voice_state_update(member, before, after):
+    if str(member.id) != "523531083078434819":  # Exclute the bot itself
+        if after.channel.name == "Heiiiiiiiiiin" and (before.channel is None or before.channel.name != "Heiiiiiiiiiin"):
             # Join the channel
-            print(member,"join the channel HEEEEEEEEEIIIIIIIIIINNNN")
-
+            print(member, "join the channel HEEEEEEEEEIIIIIIIIIINNNN")
 
             channel = after.channel
             vc = await channel.connect()
 
             try:
-                vc.play(discord.FFmpegPCMAudio('hein.mp3'), after=lambda e: print('Hein done'))
+                vc.play(discord.FFmpegPCMAudio(
+                    source='hein.mp3',
+                    executable=r'D:\Program Files\ffmpeg\ffmpeg-5.0.1-essentials_build\bin\ffmpeg.exe'),
+                    after=lambda x: print('Hein done')
+                )
                 time.sleep(2)
                 await vc.disconnect()
 
--- callbacks.py
+++ callbacks.py
@@ -4,126 +4,32 @@ import discord
 import sqlite3
 import time
 
-import rlstats
 
-async def randomNickname(message,guild):
-    splitedString = message.content.split(" ",1)
-    if len(splitedString)==2 :
-        memberId=0
-        if splitedString[1].startswith("<@!"):
-            # remove 3 first char and last one
-            temp = splitedString[1][3:]
-            memberId = temp[:-1]
-
-        for member in message.guild.members:
-            # If start with @ remove the @
-            if (member.name.lower() == splitedString[1].lower()) or (str(member.id) == str(memberId)) or (member.display_name.lower() == splitedString[1].lower()) :
-                output = "Renaming "+member.name
-                # await message.channel.send(output)
-                await message.delete()
-
-                originalName = member.display_name.lower()
-                shuffled = list(originalName)
-                random.shuffle(shuffled)
-                shuffled = ''.join(shuffled)
-                shuffled = shuffled.capitalize()
-                await member.edit(nick=shuffled,reason="Heeeeeeeeiiiin")
-                break;
-        else :
-            await message.channel.send("User not found")
-            pass
-
-    else :
-        await message.channel.send("Bad parameters")
-        pass
-
-async def resetNickname(message,guild):
-    splitedString = message.content.split(" ",1)
-    if len(splitedString)==2 :
-        memberId=0
-        if splitedString[1].startswith("<@!"):
-            # remove 3 first char and last one
-            temp = splitedString[1][3:]
-            memberId = temp[:-1]
-
-        for member in message.guild.members:
-            # If start with @ remove the @
-            if (member.name.lower() == splitedString[1].lower()) or (str(member.id) == str(memberId)) or (member.display_name.lower() == splitedString[1].lower()) :
-                await message.delete()
-
-                # Connect to DB
-                conn = sqlite3.connect('users.db')
-                c = conn.cursor()
-
-                c.execute("SELECT name, display_name FROM users WHERE id=?",(member.id,))
-                values = c.fetchone()
-
-                nickname = values[1]
-                oldNickname = member.display_name
-                # If current nickname is different than saved nickname
-                if oldNickname != nickname :
-                    await member.edit(nick=nickname,reason="Heeeeeeeeiiiin")
-                    msg = "Reset" + oldNickname + ' :arrow_forward: ' +nickname
-                    await message.channel.send(content=msg,delete_after=5)
-
-                break;
-        else :
-            pass
-
-    else :
-        await message.channel.send("Bad parameters")
-        pass
-
-async def setNickname(message,guild):
-    splitedString = message.content.split()
-    if len(splitedString)==3 :
-        memberId=0
-        if splitedString[1].startswith("<@!"):
-            # remove 3 first char and last one
-            temp = splitedString[1][3:]
-            memberId = temp[:-1]
-
-        for member in message.guild.members:
-            # If start with @ remove the @
-            if (member.name.lower() == splitedString[1].lower()) or (str(member.id) == str(memberId)) or (member.display_name.lower() == splitedString[1].lower()) :
-                await message.delete()
-                await member.edit(nick=splitedString[2],reason="Heeeeeeeeiiiin")
-                break;
-        else :
-            await message.channel.send("User not found")
-            pass
-
-    else :
-        await message.channel.send("Bad parameters")
-        pass
-
-async def hein(message,guild):
-
+async def hein(message, guild):
     splitedString = message.content.split()
 
-    if len(splitedString)==1 :
+    if len(splitedString) == 1:
         # If only $hein
-        msg = '<:Heeee:723830564104437841> <:eeee:723833827390128140> <:eeee:723833827390128140> <:eeee:723833827390128140> <:ein:723833854703697921>'.format(message)
+        msg = '<:Heeee:723830564104437841> <:eeee:723833827390128140> <:eeee:723833827390128140> <:eeee:723833827390128140> <:ein:723833854703697921>'.format(
+            message)
         await message.delete()
-        await message.channel.send(msg,delete_after=30)
+        await message.channel.send(msg, delete_after=30)
 
 
-
-    else :
+    else:
         # hein on a user in a voice channel
         # $hein @Nyitus
         # or everybody $hein all
-        if splitedString[1] == "all" :
+        if splitedString[1] == "all":
             await message.delete()
             # Se connecte a chaque channel où il y a au moins une personne et crie HEEEEIIIINNNN
             channels = guild.voice_channels
             notEmptyChannel = []
             for channel in channels:
                 members = channel.members
-                if len(members)>=1 :
-                    print("Channel :",channel)
+                if len(members) >= 1:
+                    print("Channel :", channel)
                     notEmptyChannel.append(channel)
-
 
             for channel in notEmptyChannel:
                 vc = await channel.connect()
@@ -136,18 +42,18 @@ async def hein(message,guild):
                 except Exception as e:
                     print(e)
                     await vc.disconnect()
-        else :
-            memberId=0
+        else:
+            memberId = 0
             if splitedString[1].startswith("<@!"):
                 # remove 3 first char and last one
                 # This happens when you @ someone
                 temp = splitedString[1][3:]
                 memberId = temp[:-1]
 
-
             for member in message.guild.members:
                 # If start with @ remove the @
-                if (member.name.lower() == splitedString[1].lower()) or (str(member.id) == str(memberId)) or (member.display_name.lower() == splitedString[1].lower()) :
+                if (member.name.lower() == splitedString[1].lower()) or (str(member.id) == str(memberId)) or (
+                        member.display_name.lower() == splitedString[1].lower()):
                     # User found
                     await message.delete()
 
@@ -158,8 +64,8 @@ async def hein(message,guild):
                     for channel in channels:
                         members = channel.members
                         for memberConnected in members:
-                            if memberConnected==member:
-                                print("Member found in channel :",channel)
+                            if memberConnected == member:
+                                print("Member found in channel :", channel)
                                 flag = False
                                 vc = await channel.connect()
 
@@ -173,83 +79,27 @@ async def hein(message,guild):
                                     print(e)
                                     await vc.disconnect()
                     if flag:
-                        await message.channel.send(content="User not connected in a voice channel",delete_after=5)
-
-async def updateDB(message,guild):
-
-    await message.delete()
-    # await message.channel.send("User database updated")
+                        await message.channel.send(content="User not connected in a voice channel", delete_after=5)
 
 
-    # Connect to DB
-    conn = sqlite3.connect('users.db')
-    c = conn.cursor()
-
-    # delete all rows from table
-    c.execute('DELETE FROM users;',);
-    conn.commit()
-
-    # Get all members
-    members = '\n - '.join([member.display_name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
-
-
-
-    for member in guild.members:
-        c.execute("INSERT INTO users VALUES (?,?,?)",(member.id,member.name,member.display_name))
-
-    conn.commit()
-    print("User database updated")
-
-
-
-async def resetFromDB(message,guild):
-    await message.delete()
-    print("resetFromDB Function")
-
-    # Connect to DB
-    conn = sqlite3.connect('users.db')
-    c = conn.cursor()
-
-    msg = "Reset nickname for :"
-
-    # Get difference to only update the users with a difference
-    for member in guild.members:
-        c.execute("SELECT name, display_name FROM users WHERE id=?",(member.id,))
-        values = c.fetchone()
-
-        nickname = values[1]
-        oldNickname = member.display_name
-        # If current nickname is different than saved nickname
-        if member.display_name != nickname :
-            await member.edit(nick=nickname,reason="Heeeeeeeeiiiin")
-            msg += "\n - " + oldNickname + ' :arrow_forward: ' +nickname
-
-    await message.channel.send(content=msg,delete_after=5)
-
-
-
-async def help(message,guild):
+async def help(message, guild):
     await message.delete()
     print("Help Function")
     # Load commands
     with open('command.json') as f:
-      commands = json.load(f)
+        commands = json.load(f)
 
     msg = "~~~~~~ **Commandes Bot Chourbe** ~~~~~~\n\n"
 
+    for command in commands:
+        if command["callback"] != "help":
+            if len(command["examples"]) == 1:
+                msg += ' - **' + command["name"] + '**\n' + command["description"] + '\nCommande : `' + \
+                       command["examples"][0] + "`\n\n"
 
-    for command in commands :
-        if command["callback"]!="help":
-            if len(command["examples"])==1:
-                msg += ' - **'+command["name"]+'**\n'+command["description"]+'\nCommande : `'+command["examples"][0]+"`\n\n"
-
-            if len(command["examples"])>1:
-                msg += ' - **'+command["name"]+'**\n'+command["description"]+'\nCommande : '
+            if len(command["examples"]) > 1:
+                msg += ' - **' + command["name"] + '**\n' + command["description"] + '\nCommande : '
                 for cmd in command["examples"]:
-                    msg += "\n        `"+cmd+"`"
+                    msg += "\n        `" + cmd + "`"
                 msg += "\n\n"
     await message.channel.send(content=msg)
-
-async def stats(message,guild):
-    await rlstats.call(message,guild)
